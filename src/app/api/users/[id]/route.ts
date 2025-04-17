c37ac8190694f0d4dd0c96c6e5d84c22c6498a10
Nozen <enzo.reboul345@gmail.com>
2025-04-17 20:17:03 +0200

    fix variable name

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -5,22 +5,22 @@ const usersController = new UsersController();
 
 export async function GET(req: NextRequest, context: { params: { id: string } }) {
     const { id } = await context.params;
-    const numericId = parseInt(id, 10);
+    const userId = parseInt(id, 10);
 
-    return await usersController.getUserById(req, numericId);
+    return await usersController.getUserById(req, userId);
 }
 
 export async function PUT(req: NextRequest, context: { params: { id: string } }) {
     const { id } = await context.params;
-    const idUser = parseInt(id);
+    const userId = parseInt(id);
     const newData = await req.json();
 
-    return await usersController.updateUser(req, idUser, newData);
+    return await usersController.updateUser(req, userId, newData);
 }
 
-export async function DELETE(req: NextRequest, context: { params: {id: string}}) {
+export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
     const { id } = await context.params;
-    const numericId = parseInt(id, 10);
+    const userId = parseInt(id, 10);
 
-    return await usersController.deleteUser(req, numericId);
+    return await usersController.deleteUser(req, userId);
 }
