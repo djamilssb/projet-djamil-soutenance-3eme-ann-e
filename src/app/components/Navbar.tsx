502c23c3a1cf6384079d4df6d6859bd1e4715066
Nozen <enzo.reboul345@gmail.com>
2025-04-17 09:37:03 +0200

    fix nav

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
-import React from "react";
 
 export function Navbar(): React.JSX.Element {
     return (
-        <nav className="mr-6 ml-6 mt-2">
-            <div className="flex items-center justify-between">
-                <Image src={"https://live.staticflickr.com/65535/54455195422_75049b52e6_m.jpg"} alt="Logo de KidsTrotter" height={140} width={140}/>
-                <h1 className="text-[var(--secondary-color)] bold">KidsTrotter</h1>
-                <button className="bg-[var(--secondary-color)] hover:bg-[var(--hover-secondary)] bold p-4 rounded">Se connecter/s'inscrire</button>
+        <nav className="mr-6 ml-6 mt-2 w-full">
+            <div className="flex items-center">
+                <Image
+                    src={"https://live.staticflickr.com/65535/54455195422_75049b52e6_m.jpg"}
+                    alt="Logo de KidsTrotter"
+                    height={140}
+                    width={140}
+                />
+                <div className="flex-1">
+                    <h1 className="text-[var(--secondary-color)] bold text-center">KidsTrotter</h1>
+                </div>
             </div>
         </nav>
-    )
+    );
 }
