d5e81678e4e5bceb43302ff2d2975276c851e2a0
Nozen <enzo.reboul345@gmail.com>
2025-04-19 01:30:19 +0200

    fix type errors at parameter in dynamics routes

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -3,23 +3,23 @@ import UsersController from "../../controllers/UsersController";
 
 const usersController = new UsersController();
 
-export async function GET(req: NextRequest, context: { params: { id: string } }) {
-    const { id } = await context.params;
+export async function GET(req: NextRequest, context: { params: Promise<{ id: string }> }) {
+    const id = await context.params.then((res) => res.id);
     const userId = parseInt(id, 10);
 
     return await usersController.getUserById(req, userId);
 }
 
-export async function PUT(req: NextRequest, context: { params: { id: string } }) {
-    const { id } = await context.params;
-    const userId = parseInt(id);
+export async function PUT(req: NextRequest, context: { params: Promise<{ id: string }> }) {
+    const id = await context.params.then((res) => res.id);
+    const userId = parseInt(id, 10);
     const newData = await req.json();
 
     return await usersController.updateUser(req, userId, newData);
 }
 
-export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
-    const { id } = await context.params;
+export async function DELETE(req: NextRequest, context: { params: Promise<{ id: string }> }) {
+    const id = await context.params.then((res) => res.id);
     const userId = parseInt(id, 10);
 
     return await usersController.deleteUser(req, userId);
