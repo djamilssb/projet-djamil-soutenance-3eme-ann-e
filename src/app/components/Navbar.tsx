da538da57f1d56e08ddf4d8be22dae1fc167204d
Nozen <enzo.reboul345@gmail.com>
2025-04-19 22:26:30 +0200

    improve first layout and create first page, in working

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export function Navbar(): React.JSX.Element {
         <nav className="mr-6 ml-6 mt-2">
             <div className="flex items-center justify-between">
                 <Image src={"https://live.staticflickr.com/65535/54455195422_75049b52e6_m.jpg"} alt="Logo de KidsTrotter" height={140} width={140}/>
-                <h1 className="text-[var(--secondary-color)] bold">KidsTrotter</h1>
+                <span id="name-app" className="text-[var(--secondary-color)] bold">KidsTrotter</span>
                 <button className="bg-[var(--secondary-color)] hover:bg-[var(--hover-secondary)] bold p-4 rounded">Se connecter/s&apos;inscrire</button>
             </div>
         </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-'use client';
+import { redirect } from "next/navigation";
 
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
-export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/home");
-  }, [router]);
-
-  return null;
+export default function app() {
+  redirect("/home");
 }
