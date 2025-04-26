e4b715c07ef7c7475d5a70ad377fd81891b03fba
Nozen <enzo.reboul345@gmail.com>
2025-04-26 02:11:33 +0200

    start sign-in page, update layout to correspond and create black container component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,13 +1,29 @@
+"use client";
+
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export function Navbar(): React.JSX.Element {
-    return (
-        <nav className="mr-6 ml-6 mt-2 levitate">
-            <div className="flex items-center justify-between">
-                <Image src={"/kt-logo.png"} alt="Logo de KidsTrotter" height={140} width={140}/>
-                <span id="name-app" className="text-[var(--tertiary-color)] bold">KidsTrotter</span>
-                <button className="bg-[var(--secondary-color)] hover:bg-[var(--hover-secondary)] bold p-4 rounded cursor-pointer">Se connecter/s&apos;inscrire</button>
-            </div>
-        </nav>
-    );
+  const loginPath = usePathname().includes("/sign-in");
+
+  return loginPath ? (
+    <></>
+  ) : (
+    <nav className="mr-6 ml-6 mt-2 levitate">
+      <div className="flex items-center justify-between">
+        <Image
+          src={"/kt-logo.png"}
+          alt="Logo de KidsTrotter"
+          height={140}
+          width={140}
+        />
+        <span id="name-app" className="text-[var(--tertiary-color)] bold">
+          KidsTrotter
+        </span>
+        <button className="bg-[var(--secondary-color)] hover:bg-[var(--hover-secondary)] bold p-4 rounded cursor-pointer">
+          Se connecter/s&apos;inscrire
+        </button>
+      </div>
+    </nav>
+  );
 }
---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import BlackContainer from "../components/BlackContainer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Connexion à KidsTrotter",
+  description: "",
+};
+
+export default function SignIn(): React.JSX.Element {
+  return (
+    <>
+      <h1 hidden>Page de connexion</h1>
+
+      <div className="flex items-center justify-center min-h-[90vh]">
+        <BlackContainer>
+          <section className="flex flex-col items-center text-white">
+            <h2 className=" text-5xl bold mb-12">Connexion</h2>
+
+            <form className="flex flex-col text-2xl">
+              <label className="mb-1" htmlFor="email">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="bg-[var(--grayed-input)] rounded w-110 h-16 p-2 mb-4"
+                required
+              />
+
+              <label className="mb-1" htmlFor="password">
+                Mot de passe
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="bg-[var(--grayed-input)] w-110 h-16 rounded p-2 mb-4"
+                required
+              />
+            </form>
+
+            <Link href={"/forget-password"}>
+              <p className="text-xl">Mot de passe oublié ?</p>
+            </Link>
+
+            <button className="bg-[var(--secondary-color)] mt-6 mb-4 p-6 text-black text-2xl bold rounded cursor-pointer">Se connecter</button>
+            <button className="bg-[var(--secondary-color)] p-6 text-black text-2xl bold rounded cursor-pointer">Créer un compte</button>
+          </section>
+        </BlackContainer>
+      </div>
+    </>
+  );
+}
