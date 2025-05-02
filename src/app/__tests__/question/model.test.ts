631d6cbbe0b3b73ada7d77ca7bf094abb10d0474
Aleksandr Harisov <aharisov@smbindustries.fr>
2025-05-02 16:30:52 +0200

    added tests for question model

---
+++ src/app/__tests__/question/model.test.ts
@@ -0,0 +1,17 @@
+import Question from "@/app/api/models/Question";
+
+describe("Question Model", () => {
+    it("should correctly initialize and return properties", () => {
+        const question = new Question({
+            id: 1,
+            idQuizz: 2,
+            orderIndex: 3,
+            content: "test content"
+        });
+
+        expect(question.getId()).toBe(1);
+        expect(question.getQuizzId).toBe(2);
+        expect(question.getOrderIndex).toBe(3);
+        expect(question.getContent).toBe("test content");
+    });
+});
