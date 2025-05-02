8578f474d9d29bcdd1506b692808064556f8e9ce
djamilssb <157473835+djamilssb@users.noreply.github.com>
2025-05-02 13:49:46 +0200

    c

--- src/app/choice-quiz/page.tsx
+++ src/app/choice-quiz/page.tsx
@@ -6,13 +6,6 @@ export default function ChoixQuizzPage(): React.JSX.Element {
     <>
       <h1 hidden>Choix du quizz</h1>
 
-      <Link href="/" className="absolute top-6 left-6 z-20">
-        <div className="arrow-left"></div>
-      </Link>
-
-      <div className="absolute top-6 right-6 z-20 w-16 h-16 rounded-full bg-yellow-400 flex items-center justify-center shadow">
-        <span className="text-2xl font-bold">/</span>
-      </div>
 
       <section className="flex flex-col items-center justify-center h-[90vh]">
         <div className="flex flex-row gap-12">
@@ -24,18 +17,6 @@ export default function ChoixQuizzPage(): React.JSX.Element {
           </Link>
         </div>
       </section>
-
-      <footer className="absolute bottom-4 left-0 right-0 flex justify-between px-8 text-white text-sm">
-        <div className="flex gap-6">
-          <Link href="#">Gestion des cookies</Link>
-          <Link href="#">Conditions générales d'utilisation</Link>
-        </div>
-        <Link href="/contact">
-          <button className="bg-yellow-400 text-black font-medium px-4 py-2 rounded">
-            Nous contacter
-          </button>
-        </Link>
-      </footer>
     </>
   );
 }
