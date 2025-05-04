6ca449f29cfef5be4b25712fe947dc29a57a0763
djamilssb <157473835+djamilssb@users.noreply.github.com>
2025-05-04 17:41:23 +0200

    c

--- src/app/choice-quiz/page.tsx
+++ src/app/choice-quiz/page.tsx
@@ -6,14 +6,18 @@ export default function ChoixQuizzPage(): React.JSX.Element {
     <>
       <h1 hidden>Choix du quizz</h1>
 
-
-      <section className="flex flex-col items-center justify-center h-[90vh]">
+      <section className="flex flex-col items-center justify-center h-[90vh] bg-[url('/globe.svg')] bg-cover bg-center">
+        <h2 className="text-white text-3xl font-bold mb-8">Définissez les thèmes :</h2>
         <div className="flex flex-row gap-12">
           <Link href="/quizz/generé">
-            <button className="quiz-button">Quizz généré</button>
+            <button className="bg-[var(--secondary-color)] hover:bg-[var(--hover-secondary)] text-white text-xl font-bold py-4 px-8 rounded">
+              Quizz généré
+            </button>
           </Link>
           <Link href="/quizz/personnalisé">
-            <button className="quiz-button">Quizz personnalisé</button>
+            <button className="bg-[var(--secondary-color)] hover:bg-[var(--hover-secondary)] text-white text-xl font-bold py-4 px-8 rounded">
+              Quizz personnalisé
+            </button>
           </Link>
         </div>
       </section>
