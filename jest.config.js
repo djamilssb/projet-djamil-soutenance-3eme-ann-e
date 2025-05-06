ad1e11f9b9a4dce079227395b2c50fe6a894e575
djamilssb <157473835+djamilssb@users.noreply.github.com>
2025-05-06 22:38:05 +0200

    tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.json");
+
 module.exports = {
   testEnvironment: "node",
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" }),
 };
