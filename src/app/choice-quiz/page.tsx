d8f0a9f134f671679d415e94a85a4db4ae293c4c
djamilssb <157473835+djamilssb@users.noreply.github.com>
2025-05-09 16:05:11 +0200

    k

--- src/app/choice-quiz/page.tsx
+++ src/app/choice-quiz/page.tsx
@@ -1,12 +1,25 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 export default function ChoixQuizzPage(): React.JSX.Element {
   return (
     <>
-      <h1 hidden>Choix du quizz</h1>
+      <h1>Choix du quizz</h1>
 
-      <section className="flex flex-col items-center justify-center h-[90vh] bg-[url('/globe.svg')] bg-cover bg-center">
+      <Link href="/home">
+        <div className="absolute top-4 left-4 cursor-pointer flex items-center">
+          <Image
+            src="/Left 3.png"
+            alt="Retour à l'accueil"
+            width={40}
+            height={40}
+            className="w-10 h-10 transition-transform transform hover:scale-110 active:scale-95 active:brightness-75"
+          />
+        </div>
+      </Link>
+
+      <section className="flex flex-col items-center justify-center h-[90vh] bg-cover bg-center">
         <h2 className="text-white text-3xl font-bold mb-8">Définissez les thèmes :</h2>
         <div className="flex flex-row gap-12">
           <Link href="/quizz/generé">
