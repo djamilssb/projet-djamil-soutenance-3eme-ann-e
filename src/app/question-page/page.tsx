dd61fc82f81e90a955a89a824e7dd2df73c38222
Nozen <enzo.reboul345@gmail.com>
2025-05-10 14:06:57 +0200

    change background question need to fix height background

--- src/app/question-page/page.tsx
+++ src/app/question-page/page.tsx
@@ -1,17 +1,29 @@
-// app/quizz/page.tsx
-import './style.css';
+import Image from "next/image";
 
 export default function QuizzPage() {
   return (
-    <div className="quizz-page">
-      <div className="background"></div>
-      <div className="quizz-content">
+    <div className="quizz-page relative w-full">
+      {/* Image de fond */}
+      <div className="absolute top-0 left-0 w-full h-[100vh]">
+        <Image
+          src="/paris.png"
+          alt="Background de la question"
+          className="object-cover object-center"
+          fill
+          priority
+        />
+      </div>
+      {/* Contenu du quizz */}
+      <div className="quizz-content relative z-10">
         <div className="question-container">
           <p className="question-text">
             <strong>Jayson :</strong> Dans quelle ville se trouve ce monument ?
           </p>
           <div className="answer-options">
-            <p>a. Paris &nbsp;&nbsp; b. Londres &nbsp;&nbsp; c. Berlin &nbsp;&nbsp; d. Tokyo</p>
+            <p>
+              a. Paris &nbsp;&nbsp; b. Londres &nbsp;&nbsp; c. Berlin
+              &nbsp;&nbsp; d. Tokyo
+            </p>
           </div>
         </div>
         <img src="/alien1.png" alt="Personnage Alien" className="character" />
