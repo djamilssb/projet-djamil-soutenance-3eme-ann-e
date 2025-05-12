a3b53a9f2f597e3dda1fe6c34d9df4c81ecc3046
Nozen <enzo.reboul345@gmail.com>
2025-05-12 21:29:02 +0200

    fix request

--- src/app/api/savequizz/[id]/route.ts
+++ src/app/api/savequizz/[id]/route.ts
@@ -4,41 +4,23 @@ import SaveQuizzController from "../../controllers/SaveQuizzController";
 const saveQuizzController = new SaveQuizzController();
 
 export async function GET(req: NextRequest, context: { params: Promise<{ id: string }> }) {
-    try {
-        const id = parseInt((await context.params).id, 10);
-        if (isNaN(id)) {
-            return new Response(JSON.stringify({ message: "Invalid ID." }), { status: 400 });
-        }
-        return await saveQuizzController.getById(req, id);
-    } catch (error) {
-        return new Response(JSON.stringify({ message: "Internal server error." }), { status: 500 });
-    }
-}
+    const id = await context.params.then((res) => res.id);
+    const quizzId = parseInt(id, 10);
+
+    return await saveQuizzController.getById(req, quizzId);
+};
 
 export async function PUT(req: NextRequest, context: { params: Promise<{ id: string }> }) {
-    try {
-        const id = parseInt((await context.params).id, 10);
-        if (isNaN(id)) {
-            return new Response(JSON.stringify({ message: "Invalid ID." }), { status: 400 });
-        }
-        const body = await req.json();
-        if (!body.id_user || !body.id_quizz) {
-            return new Response(JSON.stringify({ message: "Invalid or incomplete data." }), { status: 400 });
-        }
-        return await saveQuizzController.update(req, id, body);
-    } catch (error) {
-        return new Response(JSON.stringify({ message: "Internal server error." }), { status: 500 });
-    }
-}
+    const id = await context.params.then((res) => res.id);
+    const quizzId = parseInt(id, 10);
+    const newData = await req.json();
+
+    return await saveQuizzController.update(req, quizzId, newData);
+};
 
 export async function DELETE(req: NextRequest, context: { params: Promise<{ id: string }> }) {
-    try {
-        const id = parseInt((await context.params).id, 10);
-        if (isNaN(id)) {
-            return new Response(JSON.stringify({ message: "Invalid ID." }), { status: 400 });
-        }
-        return await saveQuizzController.delete(req, id);
-    } catch (error) {
-        return new Response(JSON.stringify({ message: "Internal server error." }), { status: 500 });
-    }
-}
+    const id = await context.params.then((res) => res.id);
+    const quizzId = parseInt(id, 10);
+
+    return await saveQuizzController.delete(req, quizzId);
+};
