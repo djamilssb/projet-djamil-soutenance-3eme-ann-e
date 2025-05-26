01c93661b80035d292a3ae38d46f29765b01337b
Nozen <enzo.reboul345@gmail.com>
2025-05-26 15:36:46 +0200

    fix fetcher auth

--- src/utils/fetcher/auth/fetchSignIn.ts
+++ src/utils/fetcher/auth/fetchSignIn.ts
@@ -1,14 +1,15 @@
 import Auth from "@/app/api/models/Auth";
 
 const fetchSignIn = async (data: Auth): Promise<boolean> => {
-    return await fetch('/api/auth/sign-in', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    })
-    .then(async (res: Response) => {
+    try {
+        const res = await fetch('/api/auth/sign-in', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
         if (!res.ok) {
             if (res.status === 401) {
                 localStorage.removeItem('user_id');
@@ -16,9 +17,9 @@ const fetchSignIn = async (data: Auth): Promise<boolean> => {
                 console.error('Session expired or invalid credentials');
             }
             throw new Error('Network response failed');
-            }
-        const json = await res.json();
+        }
 
+        const json = await res.json();
         if (json.message.includes('successful')) {
             console.log('Authentication successful');
             localStorage.setItem('user_id', json.id);
@@ -27,11 +28,10 @@ const fetchSignIn = async (data: Auth): Promise<boolean> => {
         } else {
             throw new Error('Authentication failed');
         }
-    })
-    .catch((err: Error) => {
+    } catch (err) {
         console.error('Error:', err);
         return false;
-    });
+    }
 };
 
 export default fetchSignIn;
