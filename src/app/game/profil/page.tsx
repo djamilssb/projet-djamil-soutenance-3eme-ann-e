20546006699598c6a5d99cd63bd568a0a2368514
Djamil Oussalah <oussalahdjamil43@gmail.com>
2025-05-27 11:50:29 +0200

    correction button valider

--- src/app/game/profil/page.tsx
+++ src/app/game/profil/page.tsx
@@ -196,7 +196,6 @@ export default function CompteUser() {
         username: string;
         phone: string;
         address: string;
-        avatar_id?: number;
         password?: string;
         password_kids?: string;
         currentPassword?: string;
@@ -207,8 +206,7 @@ export default function CompteUser() {
         email: userData.email,
         username: userData.username,
         phone: userData.phone,
-        address: userData.address,
-        avatar_id: userData.avatar_id
+        address: userData.address
       };
       
       if (isPasswordChanged) {
