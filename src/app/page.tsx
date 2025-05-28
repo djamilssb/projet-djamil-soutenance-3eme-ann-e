01ed24304cc9f58248dcfc70ae4cafd22845db65
Cedric0510 <cedric.vanhove.82@gmail.com>
2025-05-28 14:58:36 +0200

    script init quiz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import { redirect } from "next/navigation";
+import { initGenericQuizzes } from "../utils/initGenericQuizzes";
 
-export default function app() {
-  redirect("/accueil")
+export default async function app() {
+  // Initialiser les quiz génériques au démarrage
+  await initGenericQuizzes();
+  
+  redirect("/accueil");
 }
